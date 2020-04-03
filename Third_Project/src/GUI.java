fe18d1f61fa43f80f996107409203d2a98649b6d
User <User@192.168.1.232>
2020-04-03 16:48:31 +0200

    Third_Project_First_Phase

---
+++ Third_Project/src/GUI.java
@@ -0,0 +1,88 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+
+
+public class GUI implements ActionListener {
+	
+	//Projekti 3 qe konsiston ne nje nderfaqe grafike GUI ne forme login e punuar nga Francesko Kola, Jozefina Blinishta, Valmir Lumaj, Ali Skuqaj dhe Jurgen Meti.
+	
+	
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton button;
+	int count = 5; 
+	Boolean flag = false;
+	
+	
+	public static void main(String[] args) {
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		frame.setSize(350, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		
+		userLabel = new JLabel("User");
+		userLabel.setBounds(10, 20, 80, 25);
+		panel.add(userLabel);
+		
+		userText = new JTextField(20);
+		userText.setBounds(100, 20, 165, 25);
+		panel.add(userText);
+		
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(10, 50, 80, 25);
+		panel.add(passwordLabel);
+		
+		passwordText = new JPasswordField();
+		passwordText.setBounds(100, 50, 165, 25);
+		panel.add(passwordText);
+		
+		button = new JButton("Login");
+		button.setBounds(10, 80, 80, 25);
+		button.addActionListener(new GUI());
+		panel.add(button);
+		
+		
+		frame.setVisible(true);
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String user = userText.getText();
+		String password = passwordText.getText();
+		
+		if(count != 0) {
+			if(user.equals("root") && password.equals("toor")) {
+				userText.setText(null);
+				passwordText.setText(null);
+				JOptionPane.showMessageDialog( null, "Login Successful!");
+			}
+			else {
+				userText.setText(null);
+				passwordText.setText(null);
+				count--;
+			}
+			
+		}
+		else {
+			System.exit(0);
+		}
+		
+		
+	}
+}
